3638de4594239b9bdd5dd5c2871f685d7c7e3ad0
Shatalin89 <Shatalin89@github.com>
2017-03-11 12:30:12 +0800

    add menu

--- proorg/clientlist/views.py
+++ proorg/clientlist/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.shortcuts import render_to_response
 from clientlist.models import Clients
+from django.template.loader import get_template
 
 # Create your views here.
 def clients(request):
@@ -9,3 +10,8 @@ def clients(request):
 
 def client(request, clients_id=1):
     return render_to_response('client.html', {'client': Clients.objects.get(id=clients_id)})
+
+def index(request):
+    t = get_template('index.html')
+    html = t.render()
+    return HttpResponse(html)
