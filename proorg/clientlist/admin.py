a793687c11923d37610af1d907dc33e7fa78710f
Shatalin89 <Shatalin89@github.com>
2017-03-11 22:20:59 +0800

    add menu

--- proorg/clientlist/admin.py
+++ proorg/clientlist/admin.py
@@ -4,6 +4,7 @@ from clientlist.models import Clients
 # Register your models here.
 
 class ClientAdmin(admin.ModelAdmin):
-    fields = ['clients_name','clients_middlename','clients_lastname','clients_age','clients_telephone']
+    fields = ['clients_name','clients_middlename','clients_lastname','clients_age','clients_telephone',
+              'clients_email']
 
 admin.site.register(Clients, ClientAdmin)
--- proorg/proorg/urls.py
+++ proorg/proorg/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from clientlist import urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('clientlist.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
